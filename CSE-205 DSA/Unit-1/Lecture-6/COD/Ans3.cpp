0cabe057fded09a446ada6b1966b87a3307f21cd
Ashmit Mehta <amehta.ashmit2@gmail.com>
2023-08-20 16:58:58 +0530

    -Added Set-2 Soln for CSE-205 Unit-1 Lec-6 COD

--- CSE-205 DSA/Unit-1/Lecture-6/COD/Ans3.cpp
+++ CSE-205 DSA/Unit-1/Lecture-6/COD/Ans3.cpp
@@ -2,34 +2,31 @@
 #include <iostream>
 using namespace std;
 
-void output(int arr[], int n){
+void output(int arr[],int n){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
-        cout<<endl;
+    cout<<endl;
 }
 
 
-int sort(int arr[],int n){
-    for(int i=0;i<n;i++){
-        bool swapped=false;
-        for(int j=0;j<n-1-i;j++){
+void sort(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-1;j++){
             if(arr[j]>arr[j+1]){
                 int temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
-                swapped=true;
             }
         }
         cout<<"Iteration "<<i+1<<": ";
         output(arr,n);
-        if(!swapped){
-            break;
-        }
     }
+    
     cout<<"Sorted array: ";
     output(arr,n);
 }
+
 
 int main(){
     
@@ -45,6 +42,7 @@ int main(){
     output(arr,n);
     
     sort(arr,n);
+    
     
     return 0;
 }
